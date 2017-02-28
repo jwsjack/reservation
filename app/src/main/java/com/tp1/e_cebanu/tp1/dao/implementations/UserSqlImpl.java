55d4849928dc1f17a42cb10bdecd629fb9909a0a
Vic <victor.gortopan@gmail.com>
2017-02-27 21:57:19 -0500

    New interfaces and implementations as example

---
+++ app/src/main/java/com/tp1/e_cebanu/tp1/dao/implementations/UserSqlImpl.java
@@ -0,0 +1,42 @@
+package com.tp1.e_cebanu.tp1.dao.implementations;
+
+import com.tp1.e_cebanu.tp1.dao.interfaces.UserDao;
+import com.tp1.e_cebanu.tp1.models.User;
+
+import java.util.List;
+
+/**
+ * Created by User on 27.02.2017.
+ */
+
+public class UserSqlImpl implements UserDao {
+    @Override
+    public void insertUser(User user) {
+
+    }
+
+    @Override
+    public void deleteUser(User user) {
+
+    }
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public User getUserByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+}
