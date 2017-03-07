11e73f78de0ed2ea2c72a9fd53469babeb9370e1
jwsjack <jwsjack3@gmail.com>
2017-03-06 20:05:03 -0500

    icon files

---
+++ app/src/main/java/com/tp1/e_cebanu/tp1/models/Reason.java
@@ -0,0 +1,66 @@
+package com.tp1.e_cebanu.tp1.models;
+/**
+ * Java# version 1.8.0
+ *
+ * @class Reason
+ * @package    TP #1 / IFT 1155 A - Programmation mobile à plateforme libre
+ * @author     EUGENIU CEBANU / matricule: 20025851
+ * @author     jwsjack3@gmail.com
+ * @version    1
+ * @date       2017-02-20
+ * @description Modele - gestion des motifs
+ */
+
+public class Reason {
+    protected String name;
+    protected int id;
+
+    // Constructeur
+    public Reason(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //Getters/setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Reason{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Reason reason = (Reason) o;
+
+        if (id != reason.id) return false;
+        return name != null ? name.equals(reason.name) : reason.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        return result;
+    }
+}
---
+++ app/src/main/java/com/tp1/e_cebanu/tp1/models/Role.java
@@ -0,0 +1,68 @@
+package com.tp1.e_cebanu.tp1.models;
+
+/**
+ * Java# version 1.8.0
+ *
+ * @class Role
+ * @package    TP #1 / IFT 1155 A - Programmation mobile à plateforme libre
+ * @author     EUGENIU CEBANU / matricule: 20025851
+ * @author     jwsjack3@gmail.com
+ * @version    1
+ * @date       2017-02-20
+ * @description Modele - gestion des rôles
+ */
+
+public class Role {
+    private String name;
+    private int id;
+
+    // Constructeur
+    public Role(int id, String title) {
+        this.name = name;
+        this.id = id;
+    }
+
+    //Getters/setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return name;
+    }
+
+    public void setTitle(String title) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "title='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != role.id) return false;
+        return name.equals(role.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + id;
+        return result;
+    }
+}
